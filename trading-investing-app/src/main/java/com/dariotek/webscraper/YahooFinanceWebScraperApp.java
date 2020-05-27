fa0334ecdc0363e9ce4230fec260f13ce36fc058
DarioTek <dmontecastro@yahoo.com>
2020-05-27 09:19:31 -0400

    Updated number format

--- trading-investing-app/src/main/java/com/dariotek/webscraper/YahooFinanceWebScraperApp.java
+++ trading-investing-app/src/main/java/com/dariotek/webscraper/YahooFinanceWebScraperApp.java
@@ -1,5 +1,7 @@
 package com.dariotek.webscraper;
 
+import java.text.NumberFormat;
+import java.util.Locale;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.dariotek.webscraper.entity.YahooFinanceStockQuoteSummary;
@@ -26,12 +28,20 @@ public class YahooFinanceWebScraperApp {
     	        Double earningsPotentialPercentage = (earningsPotentialValue / yahooFinanceStockQuoteSummary.getLivePrice()) * 100;    	            	            	        
     	        
     	        logger.info(yahooFinanceStockQuoteSummary.toString());
-    	        logger.info("EarningsPotentialValue = " + earningsPotentialValue);
-    	        logger.info("EarningsPotentialPercentage = " + earningsPotentialPercentage);
+    	        
+    	        NumberFormat nf = NumberFormat.getInstance(new Locale("en", "US"));
+    	        //nf.setMinimumFractionDigits(2);
+    	        nf.setMaximumFractionDigits(2);
+    	        logger.info("Earnings Potential Value = " + nf.format(earningsPotentialValue));
+    	        logger.info("Earnings Potential Percentage = " + nf.format(earningsPotentialPercentage));
     	        System.out.println("\n\n");
     	        
     	        //TODO: How close is the current value to 52 week low?
     	        //TODO: How close is the current value to 52 week high?
+    	        
+    	        /*
+    	         * https://dzone.com/articles/how-to-sort-objects-in-java
+    	         */
     		}
     	}
     	
